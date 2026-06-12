a7b3b54ef630d7df6cd6d0e30286f036d08c4f48
prithvishpp <prithvishp24@gmail.com>
2026-06-12 22:43:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,23 @@
 import streamlit as st
+import joblib
+import numpy as np
 
-st.title("Wind Turbine Prediction")
+model = joblib.load("wind_turbine_model.pkl")
 
-st.success("Streamlit is working successfully!")
+st.title("Wind Turbine Power Prediction")
+
+wind_speed = st.number_input("Wind Speed (m/s)", value=10.0)
+theoretical_power = st.number_input("Theoretical Power Curve (KWh)", value=1000.0)
+wind_direction = st.number_input("Wind Direction (°)", value=180.0)
+
+if st.button("Predict"):
+
+    data = np.array([
+        [wind_speed, theoretical_power, wind_direction]
+    ])
+
+    prediction = model.predict(data)
+
+    st.success(
+        f"Predicted Active Power: {prediction[0]:.2f} kW"
+    )
